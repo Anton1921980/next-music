d924affafd83d33650add3f02e678d17752df71b
unknown <mywear1@gmail.com>
2023-06-01 12:54:33 +0300

    server_works

---
+++ server/src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function start() {
+  try {
+    const PORT = process.env.PORT || 5000;
+    const app = await NestFactory.create(AppModule);
+    await app.listen(PORT, () => console.log('server started on Port: ', PORT));
+  } catch (e) {
+    console.log(e);
+  }
+}
+start();
