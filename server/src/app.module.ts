10d567e7e8e171360a76a405f14d42dccf5848a6
unknown <mywear1@gmail.com>
2023-06-03 14:52:28 +0300

    crud_tracks_comments_works

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TrackModule } from './track/track.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TrackModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://mywear1:w20YA4JNE4PTHmnv@cluster0.wliwu6q.mongodb.net/?retryWrites=true&w=majority',
+    ),
+  ],
+  // controllers: [AppController],
+  // providers: [AppService],
 })
 export class AppModule {}
